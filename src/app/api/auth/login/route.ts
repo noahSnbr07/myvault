954eae1a530fe61737dabeae2a185db56ea20b89
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-06-18 21:28:19 +0200

    removed lint errors

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function POST(_request: NextRequest): Promise<NextResponse> {
+export async function POST(): Promise<NextResponse> {
 
     return NextResponse.json({});
 }
